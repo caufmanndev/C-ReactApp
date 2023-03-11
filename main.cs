800dc070a7726ec3e3670a3b12b4fd009a56c4db
caufmanndev <76856032+caufmanndev@users.noreply.github.com>
2023-03-10 19:32:41 -0800

    Co-authored-by: Machiavellos <Machiavellos@users.noreply.github.com>

--- main.cs
+++ main.cs
@@ -1,1 +1,20 @@
 //hello
+//create a main function
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
